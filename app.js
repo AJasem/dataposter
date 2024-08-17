3f01ae08f7082787fa117c0102df21d0daf3a7a5
Ahmad Jasem <149052906+AJasem@users.noreply.github.com>
2024-08-17 15:45:53 +0200

    dataposter

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
-const url = "https://flag-gilt.vercel.app/api/challenge";
-const token = "uM0M7uypyeeHZ741XIrs9KsFOUEhxUdtXJA=";
+const url = process.env.URL;
+const token = `${process.env.TOKEN}=`;
 
 app.post("/flag", async (req, res) => {
   try {
